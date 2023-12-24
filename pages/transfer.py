734b79ced9c57308d7d96d973c942fd5a63b9e68
Данил Кузнецов <kuznetsov@ppdp.ru>
2023-12-24 06:41:21 +0300

    finale

--- pages/transfer.py
+++ pages/transfer.py
@@ -8,6 +8,60 @@ from numpy.typing import NDArray
 from streamlit.runtime.uploaded_file_manager import UploadedFile
 import os
 st.session_state.show_button=False
+import cv2
+import torch
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import json
+
+st.session_state.export_data = None
+@st.cache_data
+def get_depth_model():
+
+    model = torch.hub.load("intel-isl/MiDaS", "MiDaS_small")
+    return model
+class Midas():
+    def __init__(
+        self,
+    ):
+        
+        self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        self.model = get_depth_model()
+        self.model = self.model.to(self.device)
+        self.model = self.model.eval()
+
+        midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+        self.transform = midas_transforms.small_transform
+
+    def get_depth_map(self, image: np.ndarray) -> np.ndarray:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        input_batch = self.transform(image).to(self.device)
+        with torch.no_grad():
+            prediction = self.model(input_batch)
+
+        prediction = torch.nn.functional.interpolate(
+            prediction.unsqueeze(1),
+            size=image.shape[:2],
+            mode="bicubic",
+            align_corners=False,
+        ).squeeze()
+
+        output = prediction.cpu().numpy()
+        return output
+
+scaler = MinMaxScaler()
+
+def depth_transform(source, max_depth_th=0.7, min_depth_th=0.2, depth_model=None):
+    source_depth = depth_model.get_depth_map(source)
+    
+    source_depth = scaler.fit_transform(source_depth)
+    middleground_source = (source_depth >= max_depth_th) & (source_depth <= min_depth_th)
+    source_mask = np.where(middleground_source, source[:, :, 0], 0)
+    source_mask1 = np.where(middleground_source, source[:, :, 1], 0)
+    source_mask2 = np.where(middleground_source, source[:, :, 2], 0)
+    source = np.stack([source_mask, source_mask1, source_mask2], axis=-1 )
+    return source
+
 def full_color_transfer(source, target,clip=True, preserve_paper=True):
     # compute color statistics for the source and target images
     (lMeanSrc, lStdSrc, aMeanSrc, aStdSrc, bMeanSrc, bStdSrc) = image_stats(source)
@@ -41,9 +95,22 @@ def full_color_transfer(source, target,clip=True, preserve_paper=True):
     a = _scale_array(a, clip=clip)
     b = _scale_array(b, clip=clip)
     return (l, a, b)
+def stringify_array(x):
+    return [str(i) for i in x]
 
+def get_threshold_filter(source, source_gray):
+    mask = cv2.adaptiveThreshold(source_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,11,2)
+    
+    source_mask = np.where(mask, source[:, :, 0], 0.0)
+    source_mask1 = np.where(mask, source[:, :, 1], 0.0)
+    source_mask2 = np.where(mask, source[:, :, 2], 0.0)
+    source = np.stack([source_mask, source_mask1, source_mask2], axis=-1 )
+    source = source.astype(np.float32)
 
-def color_transfer(source, target, clip=True, preserve_paper=True):
+    return source
+
+def color_transfer(source, target, clip=True, preserve_paper=True, depth_model=None):
     """
     Transfers the color distribution from the source to the target
     image using the mean and standard deviations of the L*a*b*
@@ -81,9 +148,28 @@ def color_transfer(source, target, clip=True, preserve_paper=True):
     # expects floats to be 32-bit, so use that instead of 64-bit)
 
 
+    source_gray = cv2.cvtColor(source, cv2.COLOR_BGR2GRAY)
     source = cv2.cvtColor(source, cv2.COLOR_BGR2LAB).astype("float32")
     target = cv2.cvtColor(target, cv2.COLOR_BGR2LAB).astype("float32")
+    
+    source_thresholded = get_threshold_filter(source=source, source_gray=source_gray)
+    l2, a2, b2 = full_color_transfer(source_thresholded, target)
+
     l, a, b = full_color_transfer(source, target)
+    
+    source_depth = depth_transform(source, depth_model=depth_model)
+    l1, a1, b1 = full_color_transfer(source_depth, target)
+
+    export_params = {
+        "full_source":stringify_array(image_stats(source)),
+        "depth_transformed_source":stringify_array(image_stats(source_depth)),
+        "light_transformed_source":stringify_array(image_stats(source_thresholded))
+    }    
+    st.session_state.export_data = export_params
+
+    l = np.stack([l, l1, l2], axis=-1).mean(-1)
+    a = np.stack([a, a1, a2], axis=-1).mean(-1)
+    b = np.stack([b, b1, b2], axis=-1).mean(-1)
     # merge the channels together and convert back to the RGB color
     # space, being sure to utilize the 8-bit unsigned integer data
     # type
@@ -186,13 +272,20 @@ if uploaded_file is not None:
     if os.path.exists('origin.png'):
         st.markdown("Color map found!")
         source = load_origin()
-        processed = color_transfer(source, image)
+        processed = color_transfer(source, image, depth_model=Midas())
         st.session_state.show_button = True 
     else:
         st.markdown("Color map not found, \
             make sure to press 'Save colormap' button on the first page")
 
         st.session_state.show_button = False
-
-if st.button("Show results:", disabled=not st.session_state.show_button):
-    st.image(processed, width=300)
+col1, col2 = st.columns([0.5, 0.5])
+with col1:
+    if st.button("Show results:", disabled=not st.session_state.show_button):
+        st.image(processed, width=300)
+with col2:
+    if st.session_state.export_data is not None:
+        st.download_button("Export color map:",
+                       file_name="params.json", 
+                       data=json.dumps(st.session_state.export_data),
+                       disabled=not st.session_state.show_button)
